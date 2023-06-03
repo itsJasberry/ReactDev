95e56c8932d5ef98d99a8dffefb004a38afb0934
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2023-06-03 16:55:26 +0200

    commit8

--- P17_C11_dane.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM dbo.Uzytkownik;
-GO
-
-INSERT INTO dbo.Uzytkownik
-(Imie, Nazwisko, Konto, Haslo, DataZarejestrowania)
-VALUES
-('Arya', 'Stark', 'arya9', '$2y$10$SaQvqoxvfdtLmLwBjn/cnuPCcuUJT.RFFW1XyIPgpPZxWAPBn5BGG', GETDATE()),
-('Rick', 'Sanchez', 'rickandmorty', '$2y$10$0SyMsLlRx6mF5cK3dlzK2.j4jO7OkJGrNdiR7Ot4no3KeXOwOnkg6', GETDATE())
-GO
